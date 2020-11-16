3a7a0abc968db1952872cf694fae7049720c2a5b
ByCharm <72271306+ByCharm@users.noreply.github.com>
2020-11-16 16:47:56 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,37 +2,54 @@
 # Control-P (when cursor inside function call "()") shows a list of parameters and their default values
 
 import PySimpleGUI as sg
-
+from WallpaperEngine.wallpaperEngine import *
 
 # Popups and guessing are suppressed, just getting an exception by key error
 sg.set_options(suppress_raise_key_errors=False, suppress_error_popups=True, suppress_key_guessing=True)
 # setting color of the window
 sg.theme('Dark Blue 3')
 
+listbox_array = []
+previous_picture = ""
+
 layout = [[sg.Text('History:')],
-          [sg.Listbox(values=('test1', 'test2'), size=(30, 7)),
+          [sg.Listbox(values=(), size=(30, 7), key='-LISTBOX-'),
            sg.Frame('Information', [
-               [sg.Text('Current Pic:')],
-               [sg.Text('Hello', size=(35, 1), key='-OUTPUT-')],
-               [sg.Text('Selected Timer:'), sg.Text('Pic switches in:')],
-               [sg.Input(size=(13, 1), key='-IN-'), sg.Text('Hello', size=(15, 1), key='-OUTPUT1-')]
+               [sg.Text('Current Picture:')],
+               [sg.Text(size=(35, 1), key='-PICTURE_OUTPUT-')],
+               # [sg.Text('Selected Timer:'), sg.Text('Picture switches in:')],
+               # [sg.Input(size=(13, 1), key='-TIMER_INPUT-'), sg.Text('Hello', size=(15, 1), key='-TIMER_OUTPUT-')]
            ])],
           [sg.Text('_' * 77)],
           [sg.Text('Choose your path:')],
-          [sg.Input(size=(30, 1), key='-IN1-'), sg.Button('Start', tooltip='Press this Button to start'),
+          [sg.Input(size=(30, 1), key='-PATH_INPUT-'), sg.Button('Start', tooltip='Press this Button to start'),
            sg.Button('Exit', )]]
 
-window = sg.Window('WallpaperEngine', layout, grab_anywhere=True)
+window = sg.Window('WallpaperEngine', layout, grab_anywhere=False)
 
 while True:  # Event Loop
     event, values = window.read()
-    # print(event, values) this statement shows the values of each input element
+
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
+
     if event == 'Start':
-        # the '-output-' element gets the value(s) of '-in-' element
-        window['-OUTPUT-'].update(values['-IN-'])
-        # val = values['-IN-']
+        user_input_path = values['-PATH_INPUT-']
+        current_picture = run_engine(user_input_path)
+        window['-PICTURE_OUTPUT-'].update(current_picture)
+
+        if previous_picture == "":
+            previous_picture = current_picture
+        elif current_picture != previous_picture:
+            if len(listbox_array) <= 20:
+                listbox_array.append(previous_picture)
+                window['-LISTBOX-'].update(listbox_array)
+                previous_picture = current_picture
+            else:
+                listbox_array.clear()
+                listbox_array.append(previous_picture)
+                window['-LISTBOX-'].update(listbox_array)
+                previous_picture = current_picture
 
 
 window.close()
